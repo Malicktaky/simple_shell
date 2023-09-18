0f3e4fa97244dd0aaec05af209532ebe98420e00
Kongo Malick <malicktakykongo@gmail.com>
2023-09-18 16:45:45 +0000

    Update the file

---
+++ main.h
@@ -0,0 +1,25 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <types.h>
+#include <fcntl.h>
+
+/**macros**/
+#define BUFFSIZE 32
+#define BUFFER_LEN 20
+
+/**environnement**/
+extern char **environ;
+
+/**tools.c**/
+int strcmp(const char *s1, const char *s2);
+int _putchar(char c);
+
+#endif
