1fbd457b46eb1f93f8032425421854d197a7d83e
Kongo Malick <malicktakykongo@gmail.com>
2023-09-20 22:20:28 +0000

    Update the file

--- main.h
+++ main.h
@@ -42,5 +42,6 @@ int handleBuitlin(char **args, int status);
 /**functions1.c**/
 char *_getenv(char *env_var);
 int cmd_not_found(char **args, int counter);
+int handle_path(char **args);
 
 #endif
