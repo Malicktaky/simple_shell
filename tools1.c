3a5b72ea6f8f5266da25fa589f9daf62e56f3c0d
Kongo Malick <malicktakykongo@gmail.com>
2023-09-20 10:09:32 +0000

    Update the file

--- tools1.c
+++ tools1.c
@@ -19,3 +19,74 @@ int prompt(const char *prompt, unsigned int size)
 	}
 	return (0);
 }
+
+/**
+ * _strlen - Sizes the length of a string
+ * @str: String to be sized
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *str)
+{
+	int length = 0;
+
+	if (str == NULL)
+		return (0);
+
+	while (str[length] != '\0')
+		length++;
+	return (length);
+}
+
+/**
+ * free_grid - free the allocated memory
+ * @grid: pointer to grid
+ * @heigth: Heigth of the grid
+ */
+void free_grid(char **grid, int heigth)
+{
+	int i = 0;
+
+	if (grid == NULL)
+		return;
+
+	while (i < heigth)
+	{
+		free(grid[i]);
+		i++;
+	}
+}
+
+/**
+ * is_exist_file - Verify if a file exist
+ * @path: path to the file
+ *
+ * Return: 0 if the file exist or -1 if not
+ */
+int is_exist_file(char *path)
+{
+	int status;
+
+	status = (open(path, O_RDONLY));
+	if (status != -1)
+	{
+		close(status);
+		return (0);
+	}
+	return (-1);
+}
+
+/**
+ * last_free - Free the memory for the last execution
+ * @buffer: the buffer
+ */
+void last_free(char *buffer)
+{
+	if (isatty(STDIN_FILENO))
+	{
+		_putchar('\n');
+		free(buffer);
+	}
+	if (!isatty(STDIN_FILENO))
+		free(buffer);
+}
